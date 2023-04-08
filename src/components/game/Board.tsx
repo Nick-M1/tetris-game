b4d688916cbf3796fed634cadfe97b644d9e11ab
Nick-M1 <nicholasmalloura@gmail.com>
2023-04-08 10:43:22 +0100

    initial commit

--- src/components/game/Board.tsx
+++ src/components/game/Board.tsx
@@ -11,7 +11,7 @@ const Board = ({ board }: Props) => {
     };
 
     return (
-        <div className="w-[32vh] h-[64vh] sm:w-[45vh] sm:h-[90vh] sm:m-auto grid game-board" style={boardStyles}>
+        <div className="w-[30vh] h-[60vh] sm:w-[45vh] sm:h-[90vh] sm:m-auto grid game-board" style={boardStyles}>
             {board.rows.map((row, y) =>
                 row.map((cell, x) => (
                     <BoardCell key={x * board.size.columns + x} cell={cell} />
